2cea81c83d865cb6468baa1729c64bf0a185f188
jedi95 <jedi95@gmail.com>
2020-04-02 18:11:44 -0600

    Add project files.
    Signed-off-by: jedi95 <jedi95@gmail.com>

---
+++ CrysisLaunch/Program.cs
@@ -0,0 +1,162 @@
+﻿/* MIT License
+Copyright (c) 2020 jedi95
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace CrysisLaunch
+{
+    static class Program
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,
+            uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess,
+            IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        // privileges
+        const int PROCESS_CREATE_THREAD = 0x0002;
+        const int PROCESS_QUERY_INFORMATION = 0x0400;
+        const int PROCESS_VM_OPERATION = 0x0008;
+        const int PROCESS_VM_WRITE = 0x0020;
+        const int PROCESS_VM_READ = 0x0010;
+
+        // used for memory allocation
+        const uint MEM_COMMIT = 0x00001000;
+        const uint MEM_RESERVE = 0x00002000;
+        const uint PAGE_READWRITE = 4;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Get arguments
+            String[] arguments = Environment.GetCommandLineArgs();
+
+            String args = "";
+            
+            for (int i = 0; i < arguments.Length; i++)
+            {
+                if (i != 0)
+                {
+                    args += " ";
+                }
+                args += arguments[i];
+            }
+
+            //Start Crysis Wars
+            Process proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = "Crysis.exe";
+            proc.StartInfo.Arguments = args;
+            proc.StartInfo.UseShellExecute = true;
+            proc.Start();
+
+            if (Environment.Is64BitProcess)
+            {
+                //64-bit Crysis uses a multi process startup because of some DRM nonsense.
+                //We have to wait for the real game process to start so we can inject it.
+                //Start looking for Crysis64.exe
+                WaitToInject(proc);
+            }
+            else
+            {
+                //32-bit is simple. Just inject the process we started.
+                InjectDLL(proc);
+            }
+
+            //All done
+            Application.Exit();
+        }
+
+        static void WaitToInject(Process pLauncher)
+        {
+            bool injected = false;
+            while (!injected)
+            {
+                Process[] pList = Process.GetProcessesByName("Crysis64");
+                if (pList.Length > 0) //Make sure we got a processes
+                {
+                    //Loop through processes
+                    for (int i = 0; i < pList.Length; i++)
+                    {
+                        InjectDLL(pList[i]);
+                    }
+
+                    //Processes were injected, so we're done
+                    injected = true;
+                    return;
+                }
+
+                //Check if the launcher died
+                if (pLauncher.HasExited)
+                {
+                    //If the launcher has exited for more than 10 seconds, exit
+                    if ((DateTime.Now - pLauncher.ExitTime).TotalSeconds > 10)
+                    {
+                        injected = true;
+                        return;
+                    }
+                }
+            }
+        }
+
+        static void InjectDLL(Process targetProcess)
+        {
+            // geting the handle of the process - with required privileges
+            IntPtr procHandle = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, false, targetProcess.Id);
+
+            // searching for the address of LoadLibraryA and storing it in a pointer
+            IntPtr loadLibraryAddr = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+            // name of the dll we want to inject
+            string dllName = System.IO.Directory.GetCurrentDirectory() + "\\CrysisFix.dll";
+
+            // alocating some memory on the target process - enough to store the name of the dll
+            // and storing its address in a pointer
+            IntPtr allocMemAddress = VirtualAllocEx(procHandle, IntPtr.Zero, (uint)((dllName.Length + 1) * Marshal.SizeOf(typeof(char))), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+            // writing the name of the dll there
+            UIntPtr bytesWritten;
+            WriteProcessMemory(procHandle, allocMemAddress, System.Text.Encoding.Default.GetBytes(dllName), (uint)((dllName.Length + 1) * Marshal.SizeOf(typeof(char))), out bytesWritten);
+
+            // creating a thread that will call LoadLibraryA with allocMemAddress as argument
+            CreateRemoteThread(procHandle, IntPtr.Zero, 0, loadLibraryAddr, allocMemAddress, 0, IntPtr.Zero);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CrysisFix
 Fixes the refresh rate being forced to the lowest available in the DX10 version of Crysis and Crysis Wars.
+This is the DLL component of the fix. It needs to be injected into the game process before the renderer starts up in order to work.
+
+Use CrysisLaunch.exe or CrysisWarsLaunch.exe to start the game. Any arguments will be passed through to the game itself (to load mods for example)
+
+Running the game in compatability mode may prevent this fix from working. I do not know why.
+
+Depends on the Detours library (v4.0):
+https://github.com/microsoft/detours
+
+This will need to be added to the include path before you can build from source.
